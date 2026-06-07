63d099d6a111958400a8f0ceb0e5c6a3a9efb38e
Spinto11 <sebastianpipa@unisabana.edu.co>
2026-06-07 17:18:48 -0500

    add vercel cors origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,8 +13,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://reliable-q1vys09zz-sebxspipa-1831s-projects.vercel.app",
     ],
-    allow_origin_regex=r"http://192\.168\.\d+\.\d+:3000",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
